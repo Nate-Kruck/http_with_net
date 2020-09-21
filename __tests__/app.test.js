7af5ecbd79197e30f4461992930055656bed155e
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-09-21 16:13:21 -0700

    parseRequest

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,8 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
+    it('returns true', () => {
 
+        expect('true').toEqual('true')
+    })
 });
